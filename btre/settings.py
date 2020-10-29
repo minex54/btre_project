b72219748649b7a735f1cf3ff253731119626201
Tendai Patrick Mamini <tendaimamini@gmail.com>
2020-10-30 01:42:04 +0200

    new

--- btre/settings.py
+++ btre/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     
     'django.contrib.admin',
     'django.contrib.auth',
@@ -136,3 +137,14 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL ='/media/'
+
+
+
+#Messages
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS ={
+    messages.ERROR: 'danger'
+    
+}
